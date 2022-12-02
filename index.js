a71068b5ff1756e48d4f7c6f76f119fc6afb1265
TeacherAnita <teacheranitadames@gmail.com>
2022-12-02 04:22:10 -0500

    Add code to run test to retest connectivity

---
+++ index.js
@@ -0,0 +1,7 @@
+
+// media = document.querySelector(".media");
+// media.onclick = function() {
+//     navbar = document.querySelector(".nav-bar");
+//     navBar.classlist.toggle("active");
+// }
+
